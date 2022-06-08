ea34cb0f190edeae1c30206a7a4a14343412d915
Your Name <you@example.com>
2022-06-08 02:50:35 -0700

    my commit

---
+++ 1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
